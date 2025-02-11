41036f1a5692ab0a89000587fcb2147ff426b9b7
Shriya-Shetty <shriyashetty9@gmail.com>
2025-02-11 08:43:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
 st.title('🎈 Tmrt Model Learning')
 
+st.info("This is a analysis of the dedication of tmrt members")
 st.write('Hello world!')
